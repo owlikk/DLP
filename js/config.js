e673b9b54e8df0680947b4ac5b3ed755268409a8
owlikk <ko-23-205@turan-edu.kz>
2026-04-30 02:21:36 +0500

    fix config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://lrlpwqsrcpjhtsvldizg.supabase.co";
 const SUPABASE_KEY = "sb_publishable_qFQL6nXxgj877ETr8B5hBA_ofCdJCk7";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
