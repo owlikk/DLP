44077202d01ad3fbde7f677e1a56a483def1d64a
owlikk <ko-23-205@turan-edu.kz>
2026-05-13 15:06:44 +0500

    admin-panel fix

--- js/dashboard.js
+++ js/dashboard.js
@@ -34,7 +34,7 @@ async function checkAuth() {
   currentProfile = profile;
 
   if (profile?.role === "admin") {
-    adminBtn.style.display = "block";
+    adminBtn.style.display = "flex";
   }
 }
 
